3c5c9ab9be7b96fbd826691f97b24e2a80de14da
Rafael Trujillo <raftrugon@alum.us.es>
2018-06-28 22:29:21 +0200

    Al tocar sobre los temas se cambia el tema de la pagina

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private _document ) { }
 
   ngOnInit() {
+  }
+
+  cambiarColor(tema: string) {
+    const url = `assets/css/colors/${tema}.css`;
+    this._document.getElementById('tema').setAttribute('href', url);
   }
 
 }
